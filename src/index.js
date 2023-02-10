1dcaac33c944c04f02ed9c1dd11b0a50c86d8b11
OlgaMunaieva <asyssync@gmail.com>
2023-02-10 23:26:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.getElementById('search-box');
 const countryList = document.querySelector('.country-list');
-console.log(countryList);
 const countryInfo = document.querySelector('.country-info');
 
 inputEl.addEventListener(
@@ -15,35 +14,33 @@ inputEl.addEventListener(
 );
 
 /**
- *
- * @param {*} event
- * @returns
+ * by event searches for a country by the entered letter combination
+ * @param {*} event Object
+ * @returns the function terminates if the input is cleared
  */
 function searchByContainLetters(event) {
   const name = event.target.value.trim();
-  console.log(name);
-  console.log(!name);
   if (!name) {
     countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
     return;
   } else {
     fetchCountries(name).then(actsOnTheLengthOfTheArray).catch(onError);
   }
 }
 
+/**
+ * performs different functions depending on the length of the resulting array
+ * @param {*} arrCountries Object array of objects
+ */
 function actsOnTheLengthOfTheArray(arrCountries) {
   if (arrCountries.length > 10) {
     Notiflix.Notify.info(
       'Too many matches found. Please enter a more specific name.'
     );
   } else if (arrCountries.length > 1 && arrCountries.length <= 10) {
-    console.log(arrCountries);
-    console.log(arrCountries[1].languages.map(language => language.name));
-    console.log(arrCountries[0].flag);
-    console.log(arrCountries[0].name);
     markupForManyCountries(arrCountries);
   } else if (arrCountries.length === 1) {
-    // console.log(...arrCountries[1].languages.map(language => language.name));
     markupForOneCountry(...arrCountries);
   } else {
     throw new Error('No data');
@@ -51,6 +48,7 @@ function actsOnTheLengthOfTheArray(arrCountries) {
 }
 
 function markupForManyCountries(arr) {
+  countryInfo.innerHTML = '';
   const markup = arr.reduce(
     (markup, country) => markup + createMarkup(country),
     ''
@@ -80,5 +78,7 @@ function markupForOneCountry({ name, capital, population, flag, languages }) {
 
 function onError(err) {
   console.error(err);
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
   Notiflix.Notify.failure(`Oops, there is no country with that name`);
 }
